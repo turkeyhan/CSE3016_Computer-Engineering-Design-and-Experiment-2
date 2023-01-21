dc937b846940c5a6d702a0657d6787c07ef7bdb2
turkeyhan <hski0930@gmail.com>
2023-01-21 16:28:29 +0900

    Week_2
    Week_2

---
+++ Week_2/inv_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module inv_tb;
+
+reg aa, bb;
+
+wire y, zz;
+
+inv u_inv (
+.a(aa),
+.b(bb),
+.y(y),
+.z(zz));
+
+initial begin
+            aa = 1'b0;
+            bb = 1'b1;
+end
+
+always begin
+            aa = #100 ~aa;
+            bb = #50 ~bb;
+end
+
+initial begin
+            #1000
+            $finish;
+end
+
+endmodule
